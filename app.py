a1fba5c0bd8b1ddf9b14e862954dd2acfac17433
devopsteamsdb <91658727+devopsteamsdb@users.noreply.github.com>
2024-06-19 15:44:16 +0300

    add json to from

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, url_for, request, jsonify, render_template
+from flask import Flask, redirect, url_for, request, json, jsonify, render_template
 import socket
 import random
 import os
